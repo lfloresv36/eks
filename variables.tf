34ec0ce3fb46f43747aa2d25656a4f3a06ea95e3
Luis Flores <luisflores@Luis-Flores-CL.local>
2024-12-18 12:14:56 -0300

    pruebas 10

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "eks-rdg-albv2-0741"
+  default     = "eks-rdg-albv2-1214"
 }
 
 variable "cluster_version" {
